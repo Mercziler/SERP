d537da6385000cf2168585719ff922c8e625ccef
TheLordSide <cedricaguigah@gmail.com>
2022-07-06 21:01:31 +0200

    message

--- SERP.py
+++ SERP.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 SERP = Flask(__name__)
 
 @SERP.route('/')
 def index(): 
-    return('This is index')
+    return render_template("index.html")
 
 
 
